578cc082288c091ea3756f1caee09b7cc5f28649
KeshavNgeeAnn <s10257135@connect.np.edu.sg>
2024-01-22 22:44:33 +0800

    All Classes Completed will have to add comments later. Im gonna start on basic 1

--- S10241539_PRG2Assignment/S10241539_PRG2Assignment/Cup.cs
+++ S10241539_PRG2Assignment/S10241539_PRG2Assignment/Cup.cs
@@ -8,11 +8,30 @@ namespace S10241539_PRG2Assignment
 {
     class Cup : IceCream
     {
-        public cup() { }
+        public Cup() { }
         public Cup(string option, int scoops, List<Flavour> flavours, List<Topping> toppings) : base(option, scoops, flavours, toppings) { }
         public override double CalculatePrice()
         {
-
+            double price = 0;
+            if (Scoops == 1)
+            {
+                price = 4;
+            }else if (Scoops == 2)
+            {
+                price = 5.50;
+            }else
+            {
+                price = 6.50;
+            }
+            if (Scoops > 0)
+            {
+                price += 1 * Scoops;
+            }
+            return price;
+        }
+        public override string ToString()
+        {
+            return base.ToString();
         }
     }
 }
--- S10241539_PRG2Assignment/S10241539_PRG2Assignment/PointCard.cs
+++ S10241539_PRG2Assignment/S10241539_PRG2Assignment/PointCard.cs
@@ -12,7 +12,41 @@ namespace S10241539_PRG2Assignment
         private int punchCard;
         private string tier;
 
-        public PointCard() { }
+        public int Points { get; set; }
+        public int PunchCards { get; set; }
+        public string Tier { get; set; }
 
+        public PointCard() { }
+        public PointCard(int points,  int punchCard)
+        {
+            Points = points;
+            PunchCards = punchCard;
+        }
+        public void AddPoints(int p)
+        {
+            double earned_points = p * 0.72;
+            points += p;
+        }
+        public void RedeemPoints(int p)
+        {
+            if (Points > p)
+            {
+                Points -= p;
+                Console.WriteLine($"Points Balance: {Points}");
+            }
+            else
+            {
+                Console.WriteLine("Insufficient points to make this transaction");
+            }
+        }
+        public void Punch()
+        {
+            PunchCards++;
+            if (PunchCards == 11)
+            {
+                Console.WriteLine("Congratulations! You've earned a free ice cream.");
+                PunchCards = 0;
+            }
+        }
     }
 }
